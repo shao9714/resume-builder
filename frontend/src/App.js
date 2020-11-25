0db4ec6a07700e6186c8956adf9551bade1782de
Allen <allenhe77@gmail.com>
2020-11-25 23:05:42 +0300

    styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>resume builder.</h1>
+      <Home />
     </div>
   );
 }
